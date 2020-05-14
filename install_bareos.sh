2be011a9b18ef19bbe46fdfefe3241edd941be84
Leandro Aurelio <leandro.aurelio@hotmail.com>
2020-05-14 14:44:46 -0300

    Update install_bareos.sh
    Version updated from official website

--- install_bareos.sh
+++ install_bareos.sh
@@ -1,26 +1,27 @@
 #!/bin/sh
 
-# See http://download.bareos.org/bareos/release
+# See http://download.bareos.org/bareos/release/
 # for applicable releases and distributions
 
-# DIST=Debian_10
+#DIST=Debian_10
+# or
 # DIST=Debian_9.0
-# DIST=xUbuntu_18.04
+ DIST=xUbuntu_18.04
 # DIST=xUbuntu_16.04
 
-# RELEASE=release/19.02
+RELEASE=release/19.2
+# or
 # RELEASE=release/latest
 # RELEASE=experimental/nightly
 
-
-URL=http://download.bareos.org/bareos/release/latest/xUbuntu_18.04
+URL=http://download.bareos.org/bareos/$RELEASE/$DIST
 
 # add the Bareos repository
-printf "deb $URL /\n" | sudo tee /etc/apt/sources.list.d/bareos.list
+printf "deb $URL /\n" > /etc/apt/sources.list.d/bareos.list
 
-#add packege key
-wget -q $URL /Release.key -O- | apt-key add -
+# add package key
+wget -q $URL/Release.key -O- | apt-key add -
 
-#install Bareos packages
+# install Bareos packages
 apt-get update
 apt-get install bareos bareos-database-postgresql
