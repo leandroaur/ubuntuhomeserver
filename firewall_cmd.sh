5c7febab2589c655d0c6f9a0dd2feafc8bfad6a4
Leandro Aurelio <leandro.aurelio@hotmail.com>
2021-01-12 17:10:19 -0300

    Create firewall_cmd.sh

---
+++ firewall_cmd.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Read the input
+
+echo "enter the first port"
+read first_port
+echo "enter the step"
+read step
+echo "enter the last port"
+read last_port
+
+echo "first port $first_port, step $step, last port $last_port"
+    for i in $(seq $first_port $step $last_port)
+  do
+    echo firewall-cmd --zone=public --add-port=$i/tcp --permanent
+    echo "port $i added";
+  done
+    echo firewall-cmd --reload
